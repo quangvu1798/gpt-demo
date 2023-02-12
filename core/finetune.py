7aab3335228dd0e21c55652d3bf4d1ccb9c83f58
Vũ Mạnh Quang <quang.vu.1798@gmail.com>
2023-02-12 17:05:44 +0700

    Add files via upload

--- core/finetune.py
+++ core/finetune.py
@@ -1,38 +1,23 @@
 import streamlit as st
-import openai
-import re
-import os
-
-finetune_model = {
-    'temperature': 0.5,
-    'max_tokens': 1000,
-    'model': "davinci:ft-personal-2023-02-10-04-28-17",
-    'stream': True
-}
 
 def main():
-    default_value = 'Cách khai báo loại tiền mới trên web?'
-    question = st.text_input('Câu hỏi:', default_value)
-    st.write('Trả lời:')
-    answer = st.empty()
-    answer.markdown('')
+    st.markdown(
+        '''
+        <h1 align="center">
+            Chào mừng tới trang chủ 👋
+        </h1>
+        ---
+        ''',
+        unsafe_allow_html = True,
+    )
+    with st.expander('Về trang web', True):
+        st.markdown(
+            '''
+            Team AI đã xây dựng một demo về ChatGPT
+            '''
+        )
+
     
-    prompt = question
-    if st.button('Lấy câu trả lời'):
-        with st.spinner('Đang sinh câu trả lời...'):
-            response = ''
-            openai.api_key = os.environ.get('OPENAI_KEY2')
-            for resp in openai.Completion.create(prompt = prompt, **finetune_model):
-                response += resp.choices[0].text
-                response = response.replace(r'\n', '\n\n')
-                links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
-                if len(links) > 0:
-                    for link in links:
-                        link = link.replace('[', '').replace(']', '')
-                        if link[-4:] in ['.jpg', '.png', '.jpeg', '.gif']:
-                            response = response.replace('[' + link + ']', f'![image]({link})')
-                try:
-                    answer.markdown(response)
-                except:
-                    pass
-        st.success('Đã tạo xong câu trả lời!')
+if __name__ == '__main__':
+    main()
+
--- core/gpt3_finetune.py
+++ core/gpt3_finetune.py
@@ -1,7 +1,14 @@
 import streamlit as st
-from .process import construct_prompt, COMPLETIONS_API_PARAMS
+from .process import construct_prompt
 import openai
 import re
+
+finetune_model = {
+    'temperature': 0.5,
+    'max_tokens': 1000,
+    'model': "davinci:ft-personal-2023-02-10-04-28-17",
+    'stream': True
+}
 
 def main():
     default_value = 'Cách khai báo loại tiền mới trên web?'
@@ -13,9 +20,9 @@ def main():
     prompt = construct_prompt(question)[0]
     if st.button('Lấy câu trả lời'):
         with st.spinner('Đang sinh câu trả lời...'):
-            openai.api_key = "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"
             response = ''
-            for resp in openai.Completion.create(prompt = prompt, **COMPLETIONS_API_PARAMS):
+            openai.api_key = "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"
+            for resp in openai.Completion.create(prompt = prompt, **finetune_model):
                 response += resp.choices[0].text
                 response = response.replace(r'\n', '\n\n')
                 links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response)
